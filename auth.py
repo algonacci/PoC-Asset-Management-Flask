38c02d51bff70bf3183d52fb50c3958bc0b27f11
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-03-04 18:05:52 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,12 +2,14 @@ import os
 from flask_httpauth import HTTPTokenAuth
 
 secret_key = os.getenv("SECRET_KEY", "secret")
+print(secret_key)
 
 auth = HTTPTokenAuth(scheme="Bearer")
 
 
 @auth.error_handler
 def unauthorized():
+    print(secret_key)
     return {
         "status": {
             "code": 401,
